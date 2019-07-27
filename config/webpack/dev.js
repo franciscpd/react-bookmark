d5c181d075bd353c3903bf0a51149d1c06b9f57e
Francisross Soares de Oliveira <franciscpd@gmail.com>
2019-07-26 21:50:51 -0300

    Configure Jest and fix webpack run project

--- config/webpack/dev.js
+++ config/webpack/dev.js
@@ -1,14 +1,14 @@
-const merge = require('webpack-merge');
-const webpack = require('webpack');
-const commonConfig = require('./common');
+const merge = require("webpack-merge");
+const webpack = require("webpack");
+const commonConfig = require("./common");
 
 module.exports = merge(commonConfig, {
-  mode: 'development',
+  mode: "development",
   entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
-    './index.tsx', // the entry point of our app
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:3000",
+    "webpack/hot/only-dev-server",
+    "./index.js",
   ],
   devServer: {
     hot: true, // enable HMR on the server
@@ -16,7 +16,7 @@ module.exports = merge(commonConfig, {
     port: 3000, // port number
     historyApiFallback: true,
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   plugins: [
     new webpack.HotModuleReplacementPlugin(), // enable HMR globally
     new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
---
+++ src/pages/App.js
@@ -0,0 +1,6 @@
+import { hot } from "react-hot-loader/root";
+import React from "react";
+
+const App = () => <h1>React bookmark - Teste</h1>;
+
+export default hot(App);
